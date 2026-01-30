f43532186d47a56aa75ec00b17bfc0fd17f62ecd
Saim Kazmi <saimkazmi017@gmail.com>
2026-01-30 10:40:39 +0530

    Second Exercise on variables

---
+++ Exercise_2_2.py
@@ -0,0 +1,6 @@
+#Simple Messages: Assign a message to a variable, and print that message. Then
+#change the value of the variable to a new message, and print the new message.
+message = "How its going?"
+print(message)
+message = "not so good...."
+print(message)
