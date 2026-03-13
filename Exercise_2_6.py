fc0939f5415c786898d7ef96b02433fcc6de1a74
Saim Kazmi <saimkazmi017@gmail.com>
2026-03-13 18:25:13 +0530

    2-6 exercise completed

---
+++ Exercise_2_6.py
@@ -0,0 +1,6 @@
+# 2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous person’s
+# name using a variable called famous_person. Then compose your message and represent
+# it with a new variable called message. Print your message
+famous_person = "Maya Angelou"
+message = f'Once {famous_person} said,"There is no greater agony than bearing an untold story inside you"'
+print(message)
