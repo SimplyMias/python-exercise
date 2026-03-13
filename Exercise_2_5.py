b4fe5fa02611d11ef75c2c4b8d09f490891a99d1
Saim Kazmi <saimkazmi017@gmail.com>
2026-03-13 18:20:05 +0530

    2-5 exercise completed

---
+++ Exercise_2_5.py
@@ -0,0 +1,4 @@
+# 2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote
+# and the name of its author. Your output should look something like the following,
+# including the quotation marks
+print('Oscar Wilde once said,"Be yourself; everyone else is already taken"')
