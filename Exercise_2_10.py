96c2c17521fb0d38c59033bba2201de23dadda5f
Saim Kazmi <saimkazmi017@gmail.com>
2026-03-14 15:42:26 +0530

    Exercise 2 10 done

---
+++ Exercise_2_10.py
@@ -0,0 +1,11 @@
+# 2-10. Adding Comments: Choose two of the programs you’ve written, and add at least
+# one comment to each. If you don’t have anything specific to write because your programs
+# are too simple at this point, just add your name and the current date at the top of each
+# program file. Then write one sentence describing what the program does
+# Saim Kazmi
+# 3/14/2026
+message = "How its going?"
+print(message)
+message = "not so good...."
+print(message)
+# this program first print a message then we change the content of the message variable then again print the message
