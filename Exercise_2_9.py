5f799bd9b64b8596a79a8ce122e2a097eb04f1af
Saim Kazmi <saimkazmi017@gmail.com>
2026-03-14 15:38:42 +0530

    Exercise 2 9 done

---
+++ Exercise_2_9.py
@@ -0,0 +1,5 @@
+# 2-9. Favorite Number: Use a variable to represent your favorite number. Then, using
+# that variable, create a message that reveals your favorite number. Print that message
+favourite_number = 11
+message = f"My favourite number is {favourite_number}"
+print(message)
