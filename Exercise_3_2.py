ff98b30329963e639b10b6627d2d1a0bd44f7728
Saim Kazmi <saimkazmi017@gmail.com>
2026-03-14 16:22:03 +0530

    Exercise 3_2 and 3_3 done

---
+++ Exercise_3_2.py
@@ -0,0 +1,8 @@
+# 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing
+# each person’s name, print a message to them. The text of each message should be the
+# same, but each message should be personalized with the person’s name
+names = ["Imaad", "Arzim","Sarosh","Musheer"]
+print(f"Hello, {names[0]} How are you?")
+print(f"Hello, {names[1]} How are you?")
+print(f"Hello, {names[2]} How are you?")
+print(f"Hello, {names[3]} How are you?")
---
+++ Exercise_3_3.py
@@ -0,0 +1,7 @@
+# 3-3. Your Own List: Think of your favorite mode of transportation, such as a
+# motorcycle or a car, and make a list that stores several examples. Use your list to print a
+# series of statements about these items, such as “I would like to own a Honda motorcycle.”
+car = ["M5","defender","i10"]
+print(f"I would like to own Sedan like  {car[0].title()} ")
+print(f"I would like to own SUV like  {car[1].title()} ")
+print(f"I would like to own Coupe like  {car[2].title()} ")
