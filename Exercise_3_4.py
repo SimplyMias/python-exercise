a3b6d8197c5e9bee53d4599d55defcb71581bea6
Saim Kazmi <saimkazmi017@gmail.com>
2026-04-12 14:18:45 +0530

    Exercise 3-4 done

---
+++ Exercise_3_4.py
@@ -0,0 +1,8 @@
+# 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who would
+# you invite? Make a list that includes at least three people you’d like to invite to dinner.
+# Then use your list to print a message to each person, inviting them to dinner.
+guest = ["Imaad","Musheer","arzim","sahil"]
+print(f"Hey {guest[0].title()}, I am inviting you for a dinner at my place.")
+print(f"Hey {guest[1].title()}, I am inviting you for a dinner at my place.")
+print(f"Hey {guest[2].title()}, I am inviting you for a dinner at my place.")
+print(f"Hey {guest[3].title()}, I am inviting you for a dinner at my place.")
